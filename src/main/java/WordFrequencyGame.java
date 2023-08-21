61d693e549105921316133d9b0d8bca38b2f468d
Elaine De los Reyes <elaine.leah.marie.de.los.reyes@oocl.com>
2023-08-21 18:48:03 +0800

    refactor: changed generic ArrayList to List<WordFrequencyInfo>

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -51,9 +51,9 @@ public class WordFrequencyGame {
         Map<String, List<WordFrequencyInfo>> map = new HashMap<>();
         for (WordFrequencyInfo wordFrequencyInfo : wordFrequencyInfoList) {
             if (!map.containsKey(wordFrequencyInfo.getWord())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordFrequencyInfo);
-                map.put(wordFrequencyInfo.getWord(), arr);
+                List<WordFrequencyInfo> wordFrequencyInfos = new ArrayList<>();
+                wordFrequencyInfos.add(wordFrequencyInfo);
+                map.put(wordFrequencyInfo.getWord(), wordFrequencyInfos);
             } else {
                 map.get(wordFrequencyInfo.getWord()).add(wordFrequencyInfo);
             }
